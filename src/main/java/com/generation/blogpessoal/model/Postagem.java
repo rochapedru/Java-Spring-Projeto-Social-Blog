e28d74ab57bd69c65831ab454d87f77783ff53c8
Joao Pedro Oliveira Rochaq <pedrorocha2info@gmail.com>
2023-12-08 11:38:43 -0300

    primeira aula

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,72 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens") //create table tb_postagens
+
+public class Postagem {
+	
+	@Id // primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto incremento
+	private Long id;
+	
+	@Column(length = 100)
+	@NotBlank(message = "O atributo titulo é obrigatório")
+	@Size(min = 5, max = 100, message="O atributo titulo deve conter entre 5 e 100 letras")
+	private String titulo;
+	
+	@Column(length = 1000)
+	@NotBlank(message = "O atributo texto é obrigatório")
+	@Size(min = 10, max = 1000, message="O atributo texto deve conter entre 10 e 1000 letras")
+	private String texto;
+	
+	@UpdateTimestamp // a cada postagem ele atualiza a data 
+	private LocalDateTime data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	
+	
+}
